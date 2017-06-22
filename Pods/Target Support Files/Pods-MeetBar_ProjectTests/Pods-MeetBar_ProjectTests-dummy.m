81ec5b96cc6ca797b2890b290613a62485ceedea
liugongwu <liugw@mmwipo.com>
2017-06-22 14:50:36 +0800

    CYLTabBarController

---
+++ Pods/Target Support Files/Pods-MeetBar_ProjectTests/Pods-MeetBar_ProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeetBar_ProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_MeetBar_ProjectTests
+@end
---
+++ Pods/Target Support Files/Pods-MeetBar_ProjectUITests/Pods-MeetBar_ProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeetBar_ProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MeetBar_ProjectUITests
+@end
